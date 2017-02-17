6fe592a6b7438ed5b7194f10b0b80638adcd604c
James Forster <jamezx@hotmail.co.uk>
2017-02-17 01:04:22 +0000

    Added test functionality for Apache HTTP logic.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,4 +4,6 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.0"
   lazy val mockito = "org.mockito" % "mockito-core" % "1.8.5"
   lazy val sponge = "org.spongepowered" % "spongeapi" % "5.0.0"
+  lazy val apacheHttpCore = "org.apache.httpcomponents" % "httpcore" % "4.4.6"
+  lazy val apacheHttpClient = "org.apache.httpcomponents" % "httpclient" % "4.5.3"
 }

77c9181d1b9c719df3867ab4e2395d55f7b0e598
James Forster <james.forster@digital.hmrc.gov.uk>
2017-02-17 15:59:45 +0000

    Removed components no longer used.

--- src/main/scala/controllers/UserController.scala
+++ src/main/scala/controllers/UserController.scala
@@ -1,23 +1,5 @@
 package controllers
 
-import org.apache.http.{HttpRequest, HttpResponse}
-import org.apache.http.protocol._
-
 class UserController {
 
-  val httpProcessor = new HttpProcessor {
-    override def process(response: HttpResponse, context: HttpContext) = ???
-
-    override def process(request: HttpRequest, context: HttpContext) = ???
-  }
-
-  val requestHandler = new HttpRequestHandler {
-    override def handle(request: HttpRequest, response: HttpResponse, context: HttpContext) = ???
-  }
-
-  val handlerMapper = new UriHttpRequestHandlerMapper()
-
-  handlerMapper.register("/scout/online-users", requestHandler)
-
-  val httpService = new HttpService(httpProcessor, handlerMapper)
 }

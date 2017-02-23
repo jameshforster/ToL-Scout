31ed65eaf382c68ff015d93bc524f0230c006a2a
James Forster <james.forster@digital.hmrc.gov.uk>
2017-02-23 14:31:57 +0000

    Added additional listeners for server state.

--- src/main/scala/ScoutPlugin.scala
+++ src/main/scala/ScoutPlugin.scala
@@ -1,15 +1,28 @@
 import com.google.inject.{Inject, Singleton}
+import controllers.ServerController
 import org.slf4j.Logger
 import org.spongepowered.api.event.Listener
-import org.spongepowered.api.event.game.state.GameStartedServerEvent
+import org.spongepowered.api.event.game.state.{GameStartedServerEvent, GameStoppedEvent, GameStoppingEvent}
 import org.spongepowered.api.plugin.Plugin
 
 @Plugin(id = "tol_scout_plugin", name = "Scout Plugin", version = "0.1.0")
 @Singleton
-class ScoutPlugin @Inject()(logger: Logger) {
+class ScoutPlugin @Inject()(logger: Logger, serverController: ServerController) {
 
   @Listener
   def onServerStart(event: GameStartedServerEvent) {
+    serverController.startServer()
     logger.info("Scout has been deployed into the field!")
+  }
+
+  @Listener
+  def onServerStopping(event: GameStoppingEvent): Unit = {
+    serverController.stopServer()
+    logger.info("Scout is moving to extraction point.")
+  }
+
+  @Listener
+  def onServerStopped(event: GameStoppedEvent): Unit = {
+    logger.info("Scout has been extracted.")
   }
 }

91c20a21c11ca5a860970dd081ad59bdbcf4466c
James Forster <james.forster@digital.hmrc.gov.uk>
2017-02-24 14:40:23 +0000

    Added default configuration to plugin.

--- src/main/scala/config/AppConfig.scala
+++ src/main/scala/config/AppConfig.scala
@@ -1,20 +1,29 @@
 package config
 
+import java.nio.file.Path
+
 import com.google.inject.{Inject, Singleton}
 import ninja.leaping.configurate.ConfigurationNode
 import ninja.leaping.configurate.commented.CommentedConfigurationNode
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader
 import ninja.leaping.configurate.loader.ConfigurationLoader
 import org.slf4j.Logger
+import org.spongepowered.api.Sponge
 import org.spongepowered.api.config.DefaultConfig
 
 import scala.util.{Failure, Try}
 
 @Singleton
 @DefaultConfig(sharedRoot = true)
-class ApplicationConfig @Inject()(logger: Logger, loader: ConfigurationLoader[CommentedConfigurationNode]) extends AppConfig {
+class ApplicationConfig @Inject()(logger: Logger, path: Path) extends AppConfig {
 
   private val rootNode: Try[ConfigurationNode] = Try {
-    loader.load()
+    val loader: ConfigurationLoader[CommentedConfigurationNode] = HoconConfigurationLoader.builder().setPath(path).build()
+    val jarConfigFile = Sponge.getAssetManager.getAsset("defaultConfig.conf").get().getUrl
+    val defaultLoader: ConfigurationLoader[CommentedConfigurationNode] = HoconConfigurationLoader.builder().setURL(jarConfigFile).build()
+    val defaultNode = defaultLoader.load()
+
+    loader.load().mergeValuesFrom(defaultNode)
   } match {
     case Failure(exception) =>
       logger.error(exception.getMessage)
